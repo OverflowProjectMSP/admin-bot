6a8a2bae6cf7883d2a11296d3c2709f5aaccd42b
Willyamdevon <dezhintern@gmail.com>
2024-07-11 19:10:51 +0300

    aaa

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+import psycopg2
+import logging
+from dotenv import load_dotenv
